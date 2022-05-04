14afdde77017a0c04b66a3d329e728ee26c9f1ec
SlashTM <95855221+SlashTM@users.noreply.github.com>
2022-05-04 11:37:37 +0300

    Add files via upload

---
+++ Iterator.cpp
@@ -0,0 +1,76 @@
+﻿#include "List.h"
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+bool predicatePositive(int a)
+{
+    return a >= 3;
+}
+
+
+unsigned counting(ListIterator* li)
+{
+    unsigned cnt = 0;
+    if (li->operator()())
+        do {
+            cnt++;
+            cerr << li->operator*() << " ";
+        } while (li->operator++());
+        cerr << endl;
+        return cnt;
+}
+
+int main()
+{
+    srand((unsigned)time(0));
+    unsigned capacity, count;
+    cout << "Enter list capacity: ";
+    cin >> capacity;
+    while (capacity < 1)
+    {
+
+        cout << "Wrong capacity. Try Again: ";
+        cin >> capacity;
+    }
+    try
+    {
+        cout << "Enter number of elements to fill the list: ";
+        cin >> count;
+        List list(count);
+        for (auto i = 0; i < count; i++) {
+            int x = rand() % 5;
+            list.append(x);
+            cout << x << ' ';
+        }
+        cout << endl;
+
+        int itStep;
+        int itValue;
+
+        cout << "Enter step-iterator: ";
+        cin >> itStep;
+
+        ListIterator* litS3 = list.createIterator(Iterators::STEP, itStep);
+        
+        cout << "Enter value-iterator: ";
+        cin >> itValue;
+
+        ListIterator* litV4 = list.createIterator(Iterators::VALUE, itValue);
+        ListIterator* litPred = list.createIterator(Iterators::PREDICATE, predicatePositive);
+
+        cout << "Number of elements in step-list (step " << ((ListIteratorStep*)litS3)->getStep() << "): " << counting((ListIteratorStep*)litS3) << endl;
+
+        cout << "Number of elements in that list that equal " << ((ListIteratorValue*)litV4)->getValue() << " : " << counting((ListIteratorValue*)litV4) << endl;
+
+        cout << "Number of elements whose value is higher or equals 3: " << counting((ListIteratorPredicate*)litPred) << endl;
+    }
+
+    catch (exception e)
+    {
+        cout << e.what() << endl;
+    }
+
+}
+
+
---
+++ List.h
@@ -0,0 +1,222 @@
+#pragma once
+#include <list>
+using namespace std;
+class ListIterator;
+
+enum class Iterators {
+	STEP,
+	VALUE,
+	PREDICATE
+};
+
+class List
+{
+public:
+	list <int> list_;
+	list <int>::iterator rear;
+public:
+	friend class ListIterator;
+
+	List(size_t aSize) {
+		list_.resize(aSize);
+		rear = list_.begin();
+	}
+
+	virtual ~List() {}
+
+	bool append(int a) {
+		if (rear == list_.end())
+			return false;
+		list_.emplace(rear, a);
+		rear++;
+		if (rear == list_.end()){
+			list <int>::iterator temp = rear;
+			temp--;
+			list_.erase(temp);
+			return true;
+		}
+		list <int>::iterator del = rear;
+		del++;
+		list_.erase(rear);
+		rear = --del;
+		return true;
+	}
+
+	bool remove() {
+		if (rear == list_.begin())
+			return false;
+		list_.erase(--rear);
+		list_.resize(list_.size() + 1);
+		return true;
+	}
+	ListIterator* createIterator(Iterators it_, const int step_) const;
+	
+	ListIterator* createIterator(Iterators it_, bool (*predicate_)(int)) const;
+};
+
+class ListIterator
+{
+protected:
+	friend class List;
+	const List* List_;
+	list <int>::iterator temp;
+	virtual int currentValue()
+	{
+		return *(temp);
+	}
+	virtual bool first() = 0;
+	virtual bool next() = 0;
+public:
+	ListIterator(const List* aList) : List_(aList), temp(List_->rear)
+	{
+		if (List_->rear != List_->list_.begin())
+		{
+			temp--;
+			if (List_->rear == List_->list_.end())
+				temp--;
+		}
+		else
+			throw exception("Error. List is empty.");
+
+	}
+	bool operator++()
+	{
+		return next();
+	}
+	int operator*()
+	{
+		return currentValue();
+	}
+	bool operator()()
+	{
+		return first();
+	}
+};
+
+class ListIteratorStep : public ListIterator
+{
+private:
+	int step;
+
+	bool first() override {
+		temp = List_->rear;
+		temp--;
+		auto i = 0;
+
+		while (i < step)
+		{
+			i++;
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+	bool next() override
+	{
+		if (temp == List_->list_.begin())
+			return false;
+		temp--;
+		auto i = 0;
+
+		while (i < step)
+		{
+			i++;
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+public:
+	ListIteratorStep(const List* aList, int aStep = 1) : ListIterator(aList)
+	{
+		if (aStep <= 0)
+			throw exception("Error. Wrong step.");
+		step = aStep - 1;
+	}
+
+	int getStep() const
+	{
+		return step;
+	}
+};
+
+class ListIteratorPredicate : public ListIterator
+{
+private:
+	bool (*predicate)(int);
+	bool first() override {
+		temp = List_->rear;
+		temp--;
+		while (!predicate(*temp))
+		{
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+	bool next() override
+	{
+		if (temp == List_->list_.begin())
+			return false;
+		temp--;
+		while (!predicate(*temp))
+		{
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+public:
+	ListIteratorPredicate(const List* aPList, bool aPredicate(int)) : ListIterator(aPList)
+	{
+		predicate = aPredicate;
+	}
+
+};
+
+class ListIteratorValue : public ListIterator
+{
+private:
+	int value;
+
+	bool first() override
+	{
+		temp = List_->rear;
+		temp--;
+		while (*temp != value)
+		{
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+	bool next() override
+	{
+		if (temp == List_->list_.begin())
+			return false;
+		temp--;
+		while (*temp != value)
+		{
+			if (temp == List_->list_.begin())
+				return false;
+			temp--;
+		}
+		return true;
+	}
+public:
+	
+	int getValue() const
+	{
+		return value;
+	}
+
+	ListIteratorValue(const List* aList, int value_) : ListIterator(aList)
+	{
+		value = value_;
+	}
+};
